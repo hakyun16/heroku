0dc5072cdd2ddab1de5344028b3072a5f5c52038
hakyun16 <mjesj2018@gmail.com>
2019-05-23 18:47:59 +0900

    heroku deploy

--- lion_class/settings.py
+++ lion_class/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pgckd7&%-6^&pf^r#!*_6x!zat+2a*52=o%y83!_$7-_tqb0w^'
+# SECRET_KEY = 'pgckd7&%-6^&pf^r#!*_6x!zat+2a*52=o%y83!_$7-_tqb0w^'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',  'pgckd7&%-6^&pf^r#!*_6x!zat+2a*52=o%y83!_$7-_tqb0w^')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True //장고할 때 에러 알려주겠다는 말임
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
